f3f51a834a4f383bb47d0f9639c877f9faecaf90
Angel Rivera <2404388+punkdata@users.noreply.github.com>
2020-10-14 11:09:49 -0400

    Demo angel [ci skip] (#5)
    * Change pulumi compute to TypeScript demo
    
    * Change pulumi orb-snyk to TypeScript demo
    
    * Fixing up the circleci job to run pulumi up from nodejs
    
    * Fixing npm package.json files
    
    * Layering in a destroy phase
    
    * Starting to hook in acceptance tests
    
    * Bump application version
    
    * Trying the Go testing infra variant
    
    * delete files
    
    * test node update
    
    * update cont port
    
    * changing alternative testing to use new golang container
    
    * Fix the port the app is listening on
    
    * More work to get node installed for the alternative testing
    
    * Add missing test functions
    
    * Running a Pulumi login step as part of alternative testing
    
    * Install Yarn for alternative testing flow
    
    * Adding yarn to PATH
    
    * One more time with Yarn on the PATH
    
    * fixed ports in test and Docker file
    
    * changed workflow
    
    * fixed broken job
    
    * Once more with force!
    
    * added k8s pulumi sdk
    
    * setting the gcp creds for the testS
    
    * Thanks a bunch go!
    
    * added pulumi install to fix bug
    
    * Fixing the port I broke in the deployment
    
    * updated version
    
    * test the deploy
    
    * test pipeline
    
    * [ci skip] remove previous files
    
    * [ci skip] updated config
    
    * updated ver 2
    
    Co-authored-by: stack72 <public@paulstack.co.uk>
    Co-authored-by: Angel Rivera <2404388+ariv3ra@users.noreply.github.com>

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 var exports = module.exports = {};
-var versionNumber = "0.0.1"
+var versionNumber = "0.0.2"
 var content = ""
 
 function welcomeMessage(){
