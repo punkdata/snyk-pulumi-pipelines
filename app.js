79dbfa2299b0d31af849fd24c25e568b6543a965
Angel Rivera <ariv3ra@gmail.com>
2021-01-21 17:56:11 -0500

    updated app

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 var exports = module.exports = {};
-var versionNumber = "0.0.4"
+var versionNumber = "0.0.5"
 var content = ""
 
 function welcomeMessage(){
